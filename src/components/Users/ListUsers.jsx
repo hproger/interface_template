db9be64a80847bc3140813a9dc0bdd424c8955dc
Viktor Shmer <viktorshmer@gmail.com>
2020-07-19 01:40:25 +0700

    Переписал компонент ListUsers на функциональный

--- src/components/Users/ListUsers.jsx
+++ src/components/Users/ListUsers.jsx
@@ -1,53 +1,52 @@
-import React, { Component } from "react";
-class ListUsers extends Component {
-  render() {
-    return (
-      <div className="list_users">
-        <div className="table-responsive">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th>№</th>
-                <th>Имя</th>
-                <th>Действия</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.users.map(({ id, name, login }, i) => (
-                <tr key={id}>
-                  <td>{i + 1}</td>
-                  <td>{name}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="btn btn-default"
-                      onClick={() => this.props.handleEdit({ id, name, login })}
-                    >
-                      <span
-                        className="glyphicon glyphicon-edit"
-                        aria-hidden="true"
-                      ></span>
-                    </button>
+import React from "react";
 
-                    <button
-                      type="button"
-                      className="btn btn-default"
-                      onClick={() => this.props.handleRemove(id, i)}
-                    >
-                      <span
-                        className="glyphicon glyphicon-remove"
-                        aria-hidden="true"
-                      ></span>
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+const ListUsers = ({ users, handleRemove, handleEdit }) => {
+  return (
+    <div className="list_users">
+      <div className="table-responsive">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>Имя</th>
+              <th>Действия</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(({ id, name, login }, i) => (
+              <tr key={id}>
+                <td>{i + 1}</td>
+                <td>{name}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-default"
+                    onClick={() => handleEdit({ id, name, login })}
+                  >
+                    <span
+                      className="glyphicon glyphicon-edit"
+                      aria-hidden="true"
+                    ></span>
+                  </button>
+
+                  <button
+                    type="button"
+                    className="btn btn-default"
+                    onClick={() => handleRemove(id, i)}
+                  >
+                    <span
+                      className="glyphicon glyphicon-remove"
+                      aria-hidden="true"
+                    ></span>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ListUsers;
