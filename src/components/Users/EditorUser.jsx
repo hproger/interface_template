5beb6b9b068fafcc36f08c35a92c3d50de2a36b3
Viktor Shmer <viktorshmer@gmail.com>
2020-07-19 01:49:28 +0700

    Переписал компонент EditorUser на функциональный

--- src/components/Users/EditorUser.jsx
+++ src/components/Users/EditorUser.jsx
@@ -1,124 +1,118 @@
-import React, { Component } from "react";
-class EditorUser extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: this.props.user ? this.props.user.name : "",
-      login: this.props.user ? this.props.user.login : "",
-      pass: "",
-    };
-  }
+import React, { useEffect, useState } from "react";
 
-  componentWillReceiveProps = (nextProps) => {
-    this.setState({
-      name: nextProps.user ? nextProps.user.name : "",
-      login: nextProps.user ? nextProps.user.login : "",
-      pass: "",
-    });
-  };
-  handleInputChange = (event) => {
+const initialStateUser = {
+  name: "",
+  login: "",
+  pass: "",
+};
+
+const EditorUser = ({ user, title, handleHide, handleSave }) => {
+  const [state, setState] = useState(initialStateUser);
+
+  const handleInputChange = (event) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
 
-    this.setState({
+    setState({
       [name]: value,
     });
   };
-  handleResetState = () => {
-    this.setState({
-      name: "",
-      login: "",
-      pass: "",
-    });
-    this.props.handleHide();
+  const handleResetState = () => {
+    setState(initialStateUser);
+    handleHide();
   };
 
-  render() {
-    return (
-      <div className="editor_user">
-        <form method="POST">
-          <h5>▶ {this.props.title}</h5>
-          <div className="form-group row">
-            <label htmlFor="name" className="col-sm-3 col-form-label">
-              Имя
-            </label>
-            <div className="col-sm-9">
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                name="name"
-                placeholder="Иван"
-                value={this.state.name}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="login" className="col-sm-3 col-form-label">
-              Логин
-            </label>
-            <div className="col-sm-9">
-              <input
-                type="text"
-                className="form-control"
-                id="login"
-                name="login"
-                placeholder="Ivan3556"
-                value={this.state.login}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="pass" className="col-sm-3 col-form-label">
-              Пароль
-            </label>
-            <div className="col-sm-9">
-              <input
-                type="password"
-                className="form-control"
-                id="pass"
-                name="pass"
-                placeholder="*******"
-                value={this.state.pass}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
+  useEffect(() => {
+    setState({
+      name: user ? user.name : "",
+      login: user ? user.login : "",
+    });
+  }, [user]);
 
-          <div className="form-group row">
-            <div className="col-sm-6 col-xs-6">
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() =>
-                  this.props.handleSave({
-                    id: this.props.user ? this.props.user.id : 0,
-                    name: this.state.name,
-                    login: this.state.login,
-                    pass: this.state.pass,
-                  })
-                }
-              >
-                Сохранить
-              </button>
-            </div>
-            <div className="col-sm-6 col-xs-6">
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => this.handleResetState()}
-              >
-                Отмена
-              </button>
-            </div>
+  return (
+    <div className="editor_user">
+      <form method="POST">
+        <h5>▶ {title}</h5>
+        <div className="form-group row">
+          <label htmlFor="name" className="col-sm-3 col-form-label">
+            Имя
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              placeholder="Иван"
+              value={state.name}
+              onChange={handleInputChange}
+            />
           </div>
-        </form>
-      </div>
-    );
-  }
-}
+        </div>
+        <div className="form-group row">
+          <label htmlFor="login" className="col-sm-3 col-form-label">
+            Логин
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="text"
+              className="form-control"
+              id="login"
+              name="login"
+              placeholder="Ivan3556"
+              value={state.login}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="pass" className="col-sm-3 col-form-label">
+            Пароль
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="password"
+              className="form-control"
+              id="pass"
+              name="pass"
+              placeholder="*******"
+              value={state.pass}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+
+        <div className="form-group row">
+          <div className="col-sm-6 col-xs-6">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() =>
+                handleSave({
+                  id: user ? user.id : 0,
+                  name: state.name,
+                  login: state.login,
+                  pass: state.pass,
+                })
+              }
+            >
+              Сохранить
+            </button>
+          </div>
+          <div className="col-sm-6 col-xs-6">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleResetState}
+            >
+              Отмена
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default EditorUser;
