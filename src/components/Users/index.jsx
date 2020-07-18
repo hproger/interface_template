b78662ec4c7c0b2d11584bd855aab75e1a77f73a
Viktor Shmer <viktorshmer@gmail.com>
2020-07-19 01:37:59 +0700

    Переписал компонент Users на функциональный

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,72 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import routes from "../../routes";
 import axios from "axios";
 import ListUsers from "./ListUsers";
 import EditorUser from "./EditorUser";
-class Users extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      status: 0, // 0 - null, 1 - edit, 2 - add
-      editingUser: {},
-    };
-  }
+import { PAGES_ROUTE } from "../../pageRoutes";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [status, setStatus] = useState(0);
+  const [editingUser, setEditingUser] = useState(null);
+
   /**
    * GET /users/list
    * POST /users/add
    * POST /users/edit
    * POST /users/delete
    */
-  getListUser = () => {
+  const getListUser = useCallback(() => {
     axios
       .get(routes.users.list)
-      .then(({ data }) => {
-        this.setState({
-          users: data,
-        });
-      })
+      .then(({ data }) => setUsers(data))
       .catch(function (error) {
         console.log(error);
       });
+  }, []);
+
+  const handleHide = () => setStatus(0);
+
+  const handleEdit = (user) => {
+    setStatus(1);
+    setEditingUser(user);
   };
-  componentWillMount() {
-    this.getListUser();
-  }
-  handleAddUser = () => {
-    this.setState({ status: 2 });
-  };
-  handleHide = () => {
-    this.setState({ status: 0 });
-  };
-  handleEdit = (user) => {
-    //console.log(user);
-    this.setState({
-      status: 1,
-      editingUser: user,
-    });
-  };
-  handleRemove = (curID, curIndex) => {
-    console.log("Удаляем ", curID);
+
+  const handleRemove = (curID, curIndex) => {
     axios
       .post(routes.users.delete, { id: curID })
       .then(({ data }) => {
-        console.log("Пришёл ответ на удаление ", data);
         if (data.result === 1) {
-          let tmpUsers = this.state.users;
+          let tmpUsers = users;
           tmpUsers.splice(curIndex, 1);
-          this.setState({
-            users: tmpUsers,
-          });
+          setUsers(tmpUsers);
         }
       })
       .catch(function (error) {
         console.log(error);
       });
   };
-  handleSave = (user) => {
-    if (this.state.status === 1) {
+
+  const handleSave = (user) => {
+    if (status === 1) {
       axios
         .post(routes.users.edit, {
           id: user.id,
@@ -75,14 +58,13 @@ class Users extends Component {
           pass: user.pass,
         })
         .then(({ data }) => {
-          console.log("Отредактировали пользователя ", data);
-          this.getListUser();
-          this.handleHide();
+          getListUser();
+          handleHide();
         })
         .catch(function (error) {
           console.log(error);
         });
-    } else if (this.state.status === 2) {
+    } else if (status === 2) {
       axios
         .post(routes.users.add, {
           name: user.name,
@@ -90,70 +72,66 @@ class Users extends Component {
           pass: user.pass,
         })
         .then(({ data }) => {
-          console.log("Добавился пользователь ", data);
-          let tmpUsers = this.state.users;
+          let tmpUsers = users;
           tmpUsers.push({
             id: parseInt(data.id),
             name: user.name,
             login: user.login,
             pass: user.pass,
           });
-          this.setState({
-            users: tmpUsers,
-          });
+          setUsers(tmpUsers);
         })
         .catch(function (error) {
           console.log(error);
         });
     }
   };
-  render() {
-    return (
-      <div className="page_users">
-        <BreadcrumbsItem to="/users">Пользователи</BreadcrumbsItem>
-        <div className="row">
-          <div className="col-md-12">
-            <span className="add_btn" onClick={() => this.handleAddUser()}>
-              <span
-                className="glyphicon glyphicon-plus"
-                aria-hidden="true"
-              ></span>
-              &nbsp;Добавить пользователя
-            </span>
-          </div>
-        </div>
-        <div className="row">
-          <div
-            className={
-              this.state.status === 0
-                ? "col-md-12 col-sm-12"
-                : "col-md-6 col-sm-6"
-            }
-          >
-            <ListUsers
-              users={this.state.users}
-              handleEdit={this.handleEdit}
-              handleRemove={this.handleRemove}
-            />
-          </div>
-          {this.state.status > 0 && (
-            <div className="col-md-6 col-sm-6">
-              <EditorUser
-                title={
-                  this.state.status === 1
-                    ? "Редактирование пользователя"
-                    : "Новый пользователь"
-                }
-                user={this.state.status === 1 ? this.state.editingUser : null}
-                handleSave={this.handleSave}
-                handleHide={this.handleHide}
-              />
-            </div>
-          )}
+
+  useEffect(() => {
+    getListUser();
+  }, [getListUser]);
+
+  return (
+    <div className="page_users">
+      <BreadcrumbsItem to={PAGES_ROUTE.USERS}>Пользователи</BreadcrumbsItem>
+      <div className="row">
+        <div className="col-md-12">
+          <span className="add_btn" onClick={() => setStatus(2)}>
+            <span
+              className="glyphicon glyphicon-plus"
+              aria-hidden="true"
+            ></span>
+            &nbsp;Добавить пользователя
+          </span>
         </div>
       </div>
-    );
-  }
-}
+      <div className="row">
+        <div
+          className={status === 0 ? "col-md-12 col-sm-12" : "col-md-6 col-sm-6"}
+        >
+          <ListUsers
+            users={users}
+            handleEdit={handleEdit}
+            handleRemove={handleRemove}
+          />
+        </div>
+        {status > 0 && (
+          <div className="col-md-6 col-sm-6">
+            <EditorUser
+              title={
+                status === 1
+                  ? "Редактирование пользователя"
+                  : "Новый пользователь"
+              }
+              user={status === 1 ? editingUser : null}
+              handleSave={handleSave}
+              handleHide={handleHide}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Users;
